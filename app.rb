0540d038c65d737a8dab7b6a1c0ff037ff866df4
Corey Sawin <sawincp@gmail.com>
2023-04-03 14:43:00 -0400

    commit tree 571d9686ad33cd6da7e790e3bc9fd396baf7c254

--- app.rb
+++ app.rb
@@ -1,23 +1,27 @@
-# pp [{ id: 1, hello: "World" }, 
-#     { id: 2, hello: "Ruby" }, 
-#     { id: 3, hello: "Moon" }, 
-#     { id: 4, hello: "Learner" }]
+# app.rb
 
+#print
+# puts "Print:"
+# print "Hello world!"
+# print "Hello world!"
+# print "Hello world!"
+# puts
+
+# #puts
+# puts "Puts:"
+# puts "Hello world!"
+# puts "Hello world!"
+# puts "Hello world!"
+
+# #p method
+# print "p method [1,2,3]:"
 # p [1,2,3]
-# puts "Hello World!"
-# print "Pass this test, please."
-# p [1,2,3]
 
-# def add
-    
-# end
-# # 1 + 1
-# puts add + add
+# #pp method
+# print "pp method"
 
-def add num1, num2
-    puts num1
-    puts num2
-    num1 + num2
-end
+# pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
 
-puts add(1,3) + add(2,2)
+puts "Hello World!"
+print "Pass this test, please."
+p [1, 2, 3]
